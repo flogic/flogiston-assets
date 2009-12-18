6d102a7c032c1fe7a324685fc267d4b1b17d4200
Yossef Mendelssohn <ymendel@pobox.com>
2009-12-18 15:22:29 -0600

    Adding admin links for assets.
    Taking this from the blog again. Working on what to split off to a flogiston-base plugin.

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,25 @@
+module AdminHelper
+  def self.register_section(section)
+    const_set(:ADMIN_SECTIONS, []) unless const_defined?(:ADMIN_SECTIONS)
+    ADMIN_SECTIONS.push section
+    ADMIN_SECTIONS.uniq!
+  end
+
+  def admin_sections
+    ADMIN_SECTIONS
+  end
+
+  register_section('assets')
+  
+  def admin_section_links
+    admin_sections.inject([]) do |links, section|
+      plural = section
+      single = plural.singularize
+      
+      links + [
+        [plural,          send("admin_#{plural}_path")],
+        ["new #{single}", send("new_admin_#{single}_path")]
+      ]
+    end
+  end
+end
