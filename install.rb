689267110a36cbdb0a17e9dbbd18efe2b10f2542
Yossef Mendelssohn <ymendel@pobox.com>
2009-12-25 23:20:36 -0600

    *Actually* copying migrations on install.
    Committing the code instead of just the spec.

--- install.rb
+++ install.rb
@@ -12,6 +12,11 @@ def readme_contents
   IO.read(plugin_path('README.markdown'))
 end
 
+Dir[File.join(plugin_path('db/migrate'), '*.rb')].each do |migration|
+  puts "Installing plugin migration #{migration} to #{rails_path('db/migrate')}..."
+  FileUtils.copy(migration, rails_path('db/migrate'))
+end
+
 # run our Rails template to ensure needed gems and plugins are installed
 system("rake rails:template LOCATION=#{plugin_path('templates/plugin-install.rb')}")
 
