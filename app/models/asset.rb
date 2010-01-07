54df1686e8d852fb2c036eec4cc50007afcceeeb
Yossef Mendelssohn <ymendel@pobox.com>
2010-01-07 17:35:47 -0600

    Adding a uniqueness constraint on Asset handle.
    Still not required, but required to be unique if given.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,5 @@
 class Asset < ActiveRecord::Base
   has_attached_file :data
+  
+  validates_uniqueness_of :handle, :allow_blank => true
 end
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -28,4 +28,28 @@ describe Asset do
       end
     end
   end
+  
+  describe 'validations' do
+    it 'should not error on handle if given' do
+      Asset.delete_all
+      @asset = Asset.new(:handle => 'test_handle')
+      @asset.valid?
+      @asset.errors.should_not be_invalid(:handle)
+    end
+    
+    it 'should error on handle if not unique' do
+      handle = 'test_handle'
+      Asset.delete_all
+      other_asset = Asset.create!(:handle => handle)
+      @asset = Asset.new(:handle => handle)
+      @asset.valid?
+      @asset.errors.should be_invalid(:handle)
+    end
+    
+    it 'should not error on handle if none given' do
+      @asset = Asset.new(:handle => nil)
+      @asset.valid?
+      @asset.errors.should_not be_invalid(:handle)
+    end
+  end
 end
